1b45459769d2fa3bf57767781b550d5a3c18f68a
cmagallon1 <cesar9mv@gmail.com>
2020-04-28 19:34:15 -0500

    [ADD] data querys

--- start/client/src/pages/launch.jsx
+++ start/client/src/pages/launch.jsx
@@ -2,47 +2,51 @@ import React, { Fragment } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 
-import { LaunchTile, Header, Button, Loading } from '../components';
+import { Loading, Header, LaunchDetail } from '../components';
+import { ActionButton } from '../containers';
 
-const GET_LAUNCHES = gql`
-  query launchList($after: String) {
-    launches(after: $after) {
-      cursor
-      hasMore
-      launches {
-        id
-        isBooked
-        rocket {
-          id
-          name
-        }
-        mission {
-          name
-          missionPatch
-        }
-      }
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+  launch(id: $launchId) {
+    id
+    site
+    isBooked
+    rocket {
+      id
+      name
+      type
+    }
+    mission {
+      name
+      missionPatch
     }
   }
+}
 `;
 
-const Launches = () => {
-  const { data, loading, error } = useQuery(GET_LAUNCHES);
+const Launch = ({ launchId }) => {
+  const { data, loading, error } = useQuery(GET_LAUNCH_DETAILS, {
+    variables: { launchId }
+  });
 
+  console.log(error)
   if(loading) return <Loading/>;
-  if(error) return <p>Error</p>;
-  if(!data) return <p>NOT FOUND</p>;
+  if(error) return <p>ERROR: { error.message }</p>;
+  if(!data) return <p>NOT_FOUND</p>;
 
   return (
     <Fragment>
-      <Header/>
-      {
-        data.launches &&
-        data.launches.launches &&
-        data.launches.launches.map(launch => (
-          <LaunchTile key={launch.id} launch={launch} />
-        ))
-      }
+      <Header
+        image={
+          data.launch && data.launch.mission && data.launch.mission.missionPatch
+        }
+      >
+        {data && data.launch && data.launch.mission && data.launch.mission.name}
+     </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
     </Fragment>
-  )
-  
+  );
 }
+
+export default Launch;
